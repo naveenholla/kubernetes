005be3d7415417c3777dfa1939a4ee5e381f206e
Vishnu kannan <vishnuk@google.com>
2016-02-11 14:15:50 -0800

    Improve the naming of fields in ContainerImage struct used in NodeStatus.
    Signed-off-by: Vishnu kannan <vishnuk@google.com>

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -2840,8 +2840,8 @@ func (kl *Kubelet) setNodeStatusImages(node *api.Node) {
 	} else {
 		for _, image := range containerImages {
 			imagesOnNode = append(imagesOnNode, api.ContainerImage{
-				RepoTags: image.RepoTags,
-				Size:     image.Size,
+				Names:     image.RepoTags,
+				SizeBytes: image.Size,
 			})
 		}
 	}
